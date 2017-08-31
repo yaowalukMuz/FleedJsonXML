171dee019fa31b29d5aaf9f38716ea480844eac6
yaowamuz <yaowamuz.c@gmail.com>
2017-08-31 17:01:27 +0700

    create adepter listview and set restoration listview

--- app/src/main/java/com/hitachi_tstv/yodpanom/yaowaluk/myfleedjsonxml/ListViewAdapter.java
+++ app/src/main/java/com/hitachi_tstv/yodpanom/yaowaluk/myfleedjsonxml/ListViewAdapter.java
@@ -5,6 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by Yaowaluk on 31/08/2560.
@@ -37,13 +39,33 @@ public class ListViewAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+       ViewHolder holder = null;
+
+
         if(convertView == null){
 // load layout
             convertView = mInflater.inflate(R.layout.item_listview_org,null);
-        }else{
-            //restoration
-        }
+            holder = new ViewHolder();
+            holder.title = (TextView) convertView.findViewById(R.id.item_listview_title);
+            holder.description = (TextView) convertView.findViewById(R.id.item_listview_desc);
+            holder.authorImage = (ImageView) convertView.findViewById(R.id.item_listview_authorIcon);
+            holder.youtubeThumbnail = (ImageView) convertView.findViewById(R.id.item_listview_youtube_image);
+            convertView.setTag(holder);
 
+        }else{
+
+            //restoration
+            holder = (ViewHolder) convertView.getTag();
+        }
+holder.title.setText("HITACHI:::"+ String.valueOf(position));
         return convertView;
+    }
+
+    public class  ViewHolder{
+        TextView title;
+        ImageView authorImage;
+        ImageView youtubeThumbnail;
+        TextView description;
+
     }
 }
